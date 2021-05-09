7622a3fb06d5da19e8dcd460efefa0a66038acb9
turtle601 <turtle601@naver.com>
2021-05-09 17:40:08 +0900

    JUnit 다양한 기능
    org.junit.jupiter.api에 있는
    다양한 기능들에 대해 알아보았다.
    
    - BeforeAll
    - AfterAll
    - BeforeEach
    - AfterEach

--- src/test/java/com/bs/test/StudyTest.java
+++ src/test/java/com/bs/test/StudyTest.java
@@ -2,6 +2,10 @@ package com.bs.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class StudyTest {
@@ -12,5 +16,34 @@ class StudyTest {
 		assertNotNull(study);
 		System.out.println("do first test");
 	}
-
+	
+	@Test
+	void test1() {
+		System.out.println("do second test");
+		
+	}
+	
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("Before All");
+		
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("After All");
+		
+	}
+	
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("Before Each");
+		
+	}
+	
+	@AfterEach
+	void afterEach() {
+		System.out.println("After Each");
+		
+	}
 }
